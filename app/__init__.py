800cfca4b368fdb4a3293cc7de419a0a980eb83c
Blake Johnson <johnsonblake1@gmail.com>
2014-05-07 23:18:55 -0500

    bootstrap-ify

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask
+from flask.ext.bootstrap import Bootstrap
 from config import config
 
 from flask_flatpages import FlatPages
 
 pages = FlatPages()
+bootstrap = Bootstrap()
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
     pages.init_app(app)
+    bootstrap.init_app(app)
 
     from .blog import blog as blog_blueprint
     app.register_blueprint(blog_blueprint)
